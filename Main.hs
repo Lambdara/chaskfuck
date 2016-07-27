7dc41f29889485b2660e7a87a6f2193bee89a61b
Uwila <uwila@runbox.com>
2016-07-27 13:00:24 +0200

    Add code generation

--- Main.hs
+++ Main.hs
@@ -1,11 +1,35 @@
 import System.Environment
 
+memsize :: Int
+memsize = 30000
+    
 allowed :: Char -> Bool
 allowed = (flip elem) "+-<>,.[]"
           
 main :: IO ()
 main = do
-  (path:_) <- getArgs
+  (path:goal:[]) <- getArgs
   input <- readFile path
   let inputCode = filter allowed input
-  putStrLn inputCode
+  let outputCode = code inputCode
+  writeFile goal outputCode
+
+code :: String -> String
+code input =
+    "#include <stdio.h>\n" ++
+    "int memory[" ++ show memsize ++ "];\n" ++
+    "int memptr=0;\n" ++
+    "int main(void) {\n" ++
+    generate input ++
+    "}\n"
+
+generate :: String -> String
+generate [] = ""
+generate ('+':xs) = "memory[memptr] += 1;\n" ++ generate xs
+generate ('-':xs) = "memory[memptr] -= 1;\n" ++ generate xs
+generate ('>':xs) = "memptr++;\n" ++ generate xs
+generate ('<':xs) = "memptr--;\n" ++ generate xs
+generate (',':xs) = "memory[memptr] = getchar();\n" ++ generate xs
+generate ('.':xs) = "putchar(memory[memptr]);\n" ++ generate xs
+generate ('[':xs) = "while (memory[memptr] != 0) {\n" ++ generate xs
+generate (']':xs) = "}\n" ++ generate xs
